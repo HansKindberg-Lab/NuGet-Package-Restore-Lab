4a7932f4e6a9c36513df351aeccb9b69f6c31ca3
Hans Kindberg <hans.kindberg@gmail.com>
2016-06-08 14:48:15 +0200

    Add solution

---
+++ Project/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using EPiServer.Framework;
+using EPiServer.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Project
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		#region Methods
+
+		[TestMethod]
+		public void TestMethod1()
+		{
+			var log = LogManager.GetLogger(this.GetType());
+
+			log.Information("Test");
+
+			var initializationState = InitializationState.InitializeComplete;
+
+			Assert.IsTrue(initializationState.GetType() == typeof(InitializationState));
+		}
+
+		#endregion
+	}
+}
